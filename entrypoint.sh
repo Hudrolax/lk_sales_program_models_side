db669650eaac893c38ba9b4d2ef676db17ae30b7
NazarovS <s.nazarov@leskraft.ru>
2022-08-01 12:57:27 +0300

    bf

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# -p --production=True
-# -rh --redis_host="172.17.0.1"
-# -rdb --redis_db=0
+# -p --production=True // false - прогноз будет только для 4х первых значений. Для продакшина указывать true
+# -rh --redis_host="172.17.0.1" // IP Redis. 172.17.0.1 - стандарный IP внешнего хоста Docker
+# -rdb --redis_db=0 // номер базы данных Redis. 0 - дефолт.
 
 python main.py -p=true -rh="172.17.0.1" -rdb=0
--- env_example.py
+++ env_example.py
@@ -1,7 +1,14 @@
 # Keep this out of source code repository - save in a file or a database
+
+# Сервер 1С для запроса истории продаж
 SERVER_1C = ""
+# База 1С
 BASE_1C = ""
+# Роут к HTTP-сервису в 1С, который выполняет произвольный запрос. Файл с запросами queries.py.
 GET_QUERY_ROUTE = ""
+# API-ключ для 1С (проверяется HTTP-сервисом 1С)
 API_KEY = ""
+# user 1c
 USER = ""
+# password 1c
 PASSWORD = ""
